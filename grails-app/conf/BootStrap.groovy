eaf18442e0d2f464eb6e485a72ff5382690418f9
Peter Smith <pfr63@hotmail.fr>
2013-02-19 16:26:43 +0100

    Bootstrap

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -11,14 +11,17 @@ class BootStrap {
 		def adminRole = new Role(authority: 'ROLE_ADMIN').save(flush: true)
 		def userRole = new Role(authority: 'ROLE_USER').save(flush: true)
   
-		def testUser = new User(username: 'me', enabled: true, password: 'password')
+		def testUser = new User(username: 'me', enabled: true, password: 'password', name: 'ME', lastName: 'Pierre')
 		testUser.save(flush: true)
   
-		UserRole.create testUser, adminRole, true
-		
 		java.util.Date date= new java.util.Date()
 		def testQ1 = new Question(title: 'Why ?', nbView: 0, content: 'Why not ?', creationDate: new Timestamp(date.getTime()))
 		testQ1.save();
+		
+		//testUser.interactionContents = new ArrayList()
+		//testUser.interactionContents.add(testQ1);
+		
+		UserRole.create testUser, adminRole, true
 		
 		assert User.count() == 1
 		assert Role.count() == 2

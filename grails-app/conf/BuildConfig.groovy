1682fe00d719b46acbd8da2aa71583537605171c
Peter Smith <pfr63@hotmail.fr>
2013-03-11 22:01:42 +0100

    RichUII

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -42,6 +42,7 @@ grails.project.dependency.resolution = {
         runtime ":hibernate:$grailsVersion"
         runtime ":jquery:1.8.0"
         runtime ":resources:1.1.6"
+		runtime ':richui:0.8'
 
         // Uncomment these (or add new ones) to enable additional resources capabilities
         //runtime ":zipped-resources:1.0"
